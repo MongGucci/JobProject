5763db3f6b999995207ae6a3c627182207234bd4
skdbs <skdbs@DESKTOP-M6ICSUF>
2018-10-23 14:59:00 +0900

    tiles, jsp대충

--- src/job/controller/RecruitController.java
+++ src/job/controller/RecruitController.java
@@ -10,6 +10,29 @@ public class RecruitController {
 
 	@GetMapping("/select.do")
 	public String selectGetHandle() {
+		
+		
 		return "job.select";
 	}
+	
+	@GetMapping("/selectdetail.do")
+	public String selectdetailGetHandle() {
+		
+		
+		return "job.selectdetail";
+	}
+	
+	@GetMapping("/jobpost.do")
+	public String jobpostGetHandle() {
+		
+		
+		return "job.jobpost";
+	}
+	
+	@GetMapping("/comdetail.do")
+	public String comdetailGetHandle() {
+
+		return "job.comdetail";
+	}
+	
 }
